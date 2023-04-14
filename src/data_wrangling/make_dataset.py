7751d099c8b84853530efc16c3dd3184381502ff
ejallain <ejallain@gmail.com>
2023-04-14 19:46:05 +0000

    fixed download_competition_data

--- src/data_wrangling/make_dataset.py
+++ src/data_wrangling/make_dataset.py
@@ -2,25 +2,24 @@ import os
 import shutil
 from pyprojroot import here
 from dotenv import load_dotenv, find_dotenv
+
+proj_dir = here()
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+data_dir = os.path.join(proj_dir, 'data')
+raw_data_dir = os.path.join(data_dir, 'raw')
+
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 def download_competition_data(comp_name='playground-series-s3e12'):
     '''
     Downloads and extracts the data from the specified Kaggle competition name into data/raw
     '''
-    #cool little function that finds the root dir by looking for files commonly found there (like .git)
-    proj_dir = here()
-    # find .env automagically by walking up directories until it's found
-    dotenv_path = find_dotenv()
-    #load contents of the .env file as environment variables
-    load_dotenv(dotenv_path)
-    data_dir = os.path.join(proj_dir, 'data')
-    raw_data_dir = os.path.join(data_dir, 'raw')
     if not os.path.exists(raw_data_dir):
         os.makedirs(raw_data_dir)
 
     api = KaggleApi()
     api.authenticate()
     api.competition_download_files(comp_name, path=raw_data_dir)
-    shutil.unpack_archive(raw_data_dir + '\\' + comp_name + '.zip' , raw_data_dir)
-    os.remove(raw_data_dir + '\\' + comp_name + '.zip')
+    shutil.unpack_archive(raw_data_dir + '/' + comp_name + '.zip' , raw_data_dir)
+    os.remove(raw_data_dir + '/' + comp_name + '.zip')
